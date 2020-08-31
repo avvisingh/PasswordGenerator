5c2c8a3f2a3394a5f939d9e5e6bfdba1dea443c9
Avvi Singh <singh.avvi98@gmail.com>
2020-09-01 01:50:30 +1000

    added length prompt

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Implementing the writePassword function which will be called onclick 
+function writePassword() {
+  var length = prompt("How long would you like your password to be? Please enter a number between 8 and 128 (inclusive) only.");
+  if (length < 8 || length > 128) {
+    var length = prompt("Your length must be larger than, or equal to, 8 and smaller than, or equal to, 128. Please try again.");
+    console.log(length);
+  }
+
+}
