e7e4050bacf731cb88bae5a03ff944ad81ef45b5
Avvi Singh <singh.avvi98@gmail.com>
2020-09-04 23:09:11 +1000

    Script refactored with help from Strahinja

--- script.js
+++ script.js
@@ -13,12 +13,45 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// charUpperCase
+// declaring variables 
+var Charuppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var Charlowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var Charnumeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var Charspecial = ["@", "!", "#", "$", "%", "&", "?"];
+
 // Implementing the writePassword function which will be called onclick 
 function writePassword() {
-  var length = prompt("How long would you like your password to be? Please enter a number between 8 and 128 (inclusive) only.");
-  if (length < 8 || length > 128) {
-    var length = prompt("Your length must be larger than, or equal to, 8 and smaller than, or equal to, 128. Please try again.");
-    console.log(length);
-  }
+  var passwordLength = prompt("How long would you like your password to be? Please enter a number between 8 and 128 (inclusive) only.", "Enter a number here between 8 and 128 (inclusive).");
+  // I'm aware the length is a string and is implicitly cast into a number
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    var charArray = []
 
+    alert("The following questions will ask you to specify requirements for characters to include within your password. Please click on 'confirm' to answer with 'yes' and 'cancel' to answer with 'no'. You must select at least one character-type.");
+
+    var lowercase = confirm("Should your password contain lowercase letters?");
+    var specialChar = confirm("Should your password contain special characters?");
+    var uppercase = confirm("Should your password contain uppercase letters?");
+    var numeric = confirm("Should your password contain numeric values?");
+
+    if (!lowercase && !uppercase && !numeric && !specialChar) {
+      alert("You must select at least one character requirement. Please Try Again.");
+    } else {
+      charArray = charArray.concat(lowercase ? Charlowercase : []);
+      charArray = charArray.concat(uppercase ? Charuppercase : []);
+      charArray = charArray.concat(numeric ? Charnumeric : []);
+      charArray = charArray.concat(specialChar ? Charspecial : []);
+
+      var generatedPassword = "";
+
+      for (var i = 0; i < passwordLength; i++) {
+        var randomCharIndex = Math.floor(Math.random() * charArray.length);
+        var passwordChar = charArray[randomCharIndex];
+        generatedPassword += passwordChar;
+      }
+      document.getElementById("password").innerHTML = generatedPassword;
+    }
+  } else {
+    alert("You have not entered a number which is between 8 and 128.");
+  }
 }
